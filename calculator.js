6f1f62f053b476735632ee68120888b7f0d0ae0d
missKatiaPunter <katiapunter@gmail.com>
2018-02-25 16:33:14 +0000

    first calculator commit

---
+++ calculator.js
@@ -0,0 +1,140 @@
+let buttons = [
+    {
+        text: '7',
+        value: 7,
+    },
+    {
+        text: '8',
+        value: 8,
+    },
+    {
+        text: '9',
+        value: 9,
+    },
+    {
+        text: '+',
+        value: '+',
+    },
+    {
+        text: '4',
+        value: 4,
+    },
+    {
+        text: '5',
+        value: 5,
+    },
+    {
+        text: '6',
+        value: 6,
+    },
+    {
+        text: '÷',
+        value: '/',
+    },
+    {
+        text: '1',
+        value: 1,
+    },
+    {
+        text: '2',
+        value: 2,
+    },
+    {
+        text: '3',
+        value: 3,
+    },
+    {
+        text: '−',
+        value: '-',
+    },
+    {
+        text: '0',
+        value: 0,
+    },
+    {
+        text: '.',
+        value: '.',
+    },
+    {
+        text: '×',
+        value: '*',
+    },
+
+];
+let stringExpression = '';
+let accumulator = '';
+//let accumOperations = [];
+let calculator = document.getElementById('calculator');
+let calcScreen = document.createElement('div');
+let mainExpression = document.createElement('div');
+let messages = document.createElement('div');
+calcScreen.classList.add('screen-style');
+mainExpression.classList.add('main-expression');
+messages.classList.add('messages');
+let clearButton = createButton('CE');
+let equalButton = createButton('=');
+
+clearButton.addEventListener('click', function () {
+    stringExpression = '';
+    updateDisplay();
+    updateExpression(stringExpression);
+});
+
+equalButton.addEventListener('click', function () {
+    updateExpression(stringExpression +'=');
+    stringExpression = '' + eval(stringExpression);
+    updateDisplay();
+});
+
+function createButton (text) {
+    let button = document.createElement('button');
+    button.textContent = text;
+    return button;
+}
+calculator.appendChild(calcScreen);
+
+calcScreen.appendChild(mainExpression);
+calcScreen.appendChild(messages);
+
+buttons.forEach(function(item) {
+    let button = createButton(item.text);
+    button.addEventListener('click', function () {
+        if(stringExpression.length<6){
+            stringExpression += '' + item.value;
+            updateDisplay();
+            updateExpression(stringExpression);
+        }else{
+            updateExpression('Limit exceeded')
+        }
+    });
+    button.classList.add('single-button');
+    calculator.appendChild(button);
+});
+calculator.appendChild(equalButton);
+equalButton.classList.add('equal-button');
+calculator.appendChild(clearButton);
+clearButton.classList.add('clear-button');
+
+// function calculateStaff(stringExpression){
+// let arrNumPlusMin = stringExpression.match(/[+\-]?([0-9\.]+)/g);
+// let multDiv = stringExpression.match(/[*\/]/g);
+// let newArray = [arrNumPlusMin[0]];
+// for (let i=1; i<arrNumPlusMin.length; i++){
+//     if(arrNumPlusMin[i][0]==='-' || arrNumPlusMin[i][0]==='+'){
+//         let temp =[arrNumPlusMin[0], Number(substring(arrNumPlusMin[i]));
+//     }else{
+//         if(multDiv[0]==='*'){
+//             newArray.push(temp*arrNumPlusMin)
+//         }
+//     }
+// }
+// console.log(arrNumPlusMin, multDiv);
+// }
+
+function updateDisplay () {
+    mainExpression.textContent = stringExpression.substring(0,15);;
+}
+
+function updateExpression(expression) {
+    messages.textContent = expression.substring(0,15);
+}
